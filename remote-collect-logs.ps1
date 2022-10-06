ac858a7c19809df7281d2b1da5966c116ee4806a
PRINCE PEREIRA <princepereira@gmail.com>
2022-10-06 21:50:25 +0530

    Update remote-collect-logs.ps1

--- remote-collect-logs.ps1
+++ remote-collect-logs.ps1
@@ -8,7 +8,10 @@ $logPath = "AKS-Logs"
 
 
 mkdir $logPath
+ssh -o ConnectTimeout=300 -o 'ProxyCommand ssh -o ConnectTimeout=300 -p 2022 -W %h:%p azureuser@127.0.0.1' azureuser@${nodeIP} 'powershell -Command "rm aks* "'
+ssh -o ConnectTimeout=300 -o 'ProxyCommand ssh -o ConnectTimeout=300 -p 2022 -W %h:%p azureuser@127.0.0.1' azureuser@${nodeIP} 'powershell -Command "Remove-Item -r C:\k\debug\* -Exclude *.ps1,*.cmd,*.psm1 "'
 ssh -o ConnectTimeout=300 -o 'ProxyCommand ssh -o ConnectTimeout=300 -p 2022 -W %h:%p azureuser@127.0.0.1' azureuser@${nodeIP} 'powershell -Command C:\k\debug\collect-windows-logs.ps1 "'
 Start-Sleep 5
 Write-Host "#======  Copying collected windows logs ..."
 scp -o 'ProxyCommand ssh -o ConnectTimeout=300 -p 2022 -W %h:%p azureuser@127.0.0.1' azureuser@${nodeIP}:aks* $logPath
+
